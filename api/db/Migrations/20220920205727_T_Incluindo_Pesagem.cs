ce1cbff50a7b2843cf33dddf98aed357de154385
Witillan <witillan@alphasoftware.com.br>
2022-09-20 16:58:20 -0400

    Incluindo migrations com ajustes de pesagm

---
+++ api/db/Migrations/20220920205727_T_Incluindo_Pesagem.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Pesagem.Migrations
+{
+    public partial class T_Incluindo_Pesagem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
